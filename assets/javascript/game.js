833398358bf565080525ea6c8d51a7df0936aaa4
Lucio Verane <verane64@gmail.com>
2019-09-20 18:17:47 -0500

    reset function added in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,51 @@
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var win = 0;
-var lose = 0;
-var chances = 5;
+var loss = 0;
+var chances;
+var computerPick;
 
-computerPick = alphabet[Math.floor(Math.random() * alphabet.length)];
+//to make the computer cut out its guess from the array. would cut from the original array.
+// var position = [Math.floor(Math.random() * alphabet.length)]
+// computerPick = alphabet[position];
+// alphabet.splice(position,1);
 
-var reset = 0;
+// console.log("position " + position);
+// console.log("computerpick " + computerPick);
+// console.log("alphabet " + alphabet);
+
+function reset(){
+    computerPick = alphabet[Math.floor(Math.random() * alphabet.length)];
+    computerAnswer.textContent = "";
+    instructions.textContent = "";
+    userGuess.textContent = "Nothing yet...";
+    chances = 5;
+    guesses.textContent = chances;
+}
 
 document.addEventListener("DOMContentLoaded", function(event) { 
+    reset();
     document.onkeydown = function(event) {
-        if(reset === 0){
-            userPick = event.key;
-                if(chances === 5){
-                    userGuess.textContent = "";
-                }
-                userGuess.append(userPick + " ");
-                if(computerPick === userPick){
-                    computerAnswer.textContent = "You got it!"
-                    win = win + 1;
-                    wins.textContent = win;
-                    instructions.textContent = "(press any key to continue)";
-                    reset = 1;
-                }else{
-                    chances = chances - 1;
-                    if(chances === 0){
-                        computerAnswer.textContent = "The right answer was " + computerPick;
-                        lose = lose + 1;
-                        losses.textContent = lose;
-                        instructions.textContent = "(press any key to continue)";
-                        reset = 1;
-                    }
-                    guesses.textContent = chances;
-                }
+        userPick = event.key;
+        if(chances === 5){
+            userGuess.textContent = "";
+        }
+        userGuess.append(userPick + " ");
+        if(computerPick === userPick){
+            computerAnswer.textContent = "You got it!"
+            win++;
+            wins.textContent = win;
+            instructions.textContent = "(press any key to continue)";
+            reset ();
         }else{
-            computerPick = alphabet[Math.floor(Math.random() * alphabet.length)];
-            computerAnswer.textContent = "";
-            instructions.textContent = "";
-            userGuess.textContent = "Nothing yet...";
-            chances = 5;
+            chances = chances - 1;
+            if(chances === 0){
+                computerAnswer.textContent = "The right answer was " + computerPick;
+                loss++;
+                losses.textContent = loss;
+                instructions.textContent = "(press any key to continue)";
+                reset ();
+            }
             guesses.textContent = chances;
-            reset = 0;
         }
     }
 });
